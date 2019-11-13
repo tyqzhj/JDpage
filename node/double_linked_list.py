e1e7b62596dcad621928d1073d3ad50ae09c38b2
tyqzhj <18813017082@163.com>
2019-11-13 17:41:57 +0800

    2019/11/13 17:41

--- node/double_linked_list.py
+++ node/double_linked_list.py
@@ -30,5 +30,71 @@ class DLinkList(object):
             cur = cur.next
         print('')
 
-    def
+    def add(self,item):
+        'toubu charu yuansu'
+        node = Node(item)
+        if self.is_empty():
+            self._head = node
+        else:
+            node.next = self._head
+            self._head.prev = node
+            self._head = node
 
+    def append(self,item):
+        'weibu charu yuansu'
+        node = Node(item)
+        if self.is_empty():
+            self._head = node
+        else:
+            cur = self._head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
+            node.prev = cur
+
+    def search(self,item):
+        'chazhao yuansu shifou cunzai'
+        cur = selff._head
+        while cur != Node:
+            if cur.item == item:
+                return True
+            cur = cur.next
+        return False
+
+    def insert(self,pos,item):
+        'zai zhiding weizhi tianjia jiedian'
+        if pos <= 0:
+            self.add(item)
+        elif pos > (self.length()-1):
+            self.append(item)
+        else:
+            node = Node(item)
+            cur = self._head
+            count = 0
+            while count < (pos-1):
+                count += 1
+                cur = cur.next
+            node.prev = cur
+            node.next = cur.next
+            cur.next.prev = node
+            cur.next = node
+
+    def remove(self,item):
+        'shanchu yuansu'
+        if self.is_empty():
+            return
+        else:
+            cur = self._head
+            if cur.item == item:
+                if cur.next == None:
+                    self._head = Node
+                else:
+                    cur.next.prev = None
+                    self._head = cur.next
+                return
+            while cur != None:
+                if cur.item == item:
+                    cur.prev.next = cur.next
+                    cur.next.prev = cur.prev
+                    break
+                cur = cur.next
---
+++ node/swap_nodes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from linked_list import Node
+from linked_list import Linkedlist
+
+def swapNodes(self,d1,d2):
+    prevD1 = None
+    prevD2 = None
+    if d1 == d2:
+        return
+    else:
+
